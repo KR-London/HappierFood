e003103e619d2aab73ce8e229afa29b72ee8e109
Kate Roberts <ekaterina.roberts@gmail.com>
2019-04-04 09:36:56 -0700

    Initial Commit

---
+++ HappierFoods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappierFoods
+//
+//  Created by Kate Roberts on 04/04/2019.
+//  Copyright © 2019 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
