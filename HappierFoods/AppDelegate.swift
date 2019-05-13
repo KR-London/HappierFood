dcb17d28bda53f7152d4e3315246f3c24bfb341e
kate <ekaterina.roberts@gmail.com>
2019-05-13 01:21:42 +0100

    Post great MLM clean up

--- HappierFoods/AppDelegate.swift
+++ HappierFoods/AppDelegate.swift
@@ -16,9 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let font = UIFont(name: "TwCenMT-CondensedExtraBold", size: 24 )
+        
+
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: UIColor.black], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor: UIColor.gray], for: .normal)
+ 
+       // UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font : UIColor.black], for: .normal)
         return true
     }
+    
+    /// set orientations you want to be allowed in this property by default
+    var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+    }
+    
+ 
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
