7b9b58800868f6b2c5bab0ed32363ac01f1bf0be
kate <ekaterina.roberts@gmail.com>
2019-07-18 10:13:06 +0100

    Custom header view

---
+++ HappierFoods/History/WeekHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  WeekHeaderView.swift
+//  HappierFoods
+//
+//  Created by Kate Roberts on 18/07/2019.
+//  Copyright © 2019 Kate Roberts. All rights reserved.
+//
+
+import UIKit
+
+class WeekHeaderView: UICollectionReusableView {
+        
+}
